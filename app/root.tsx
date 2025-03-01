c7f5820948aca998de4a6412b02fe32be1c6fd25
sora <blue.solar1999@gmail.com>
2025-03-01 10:49:49 +0900

    ロード中のスタイル修正

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 import appStylesHref from "./app.css?url";
 import { LinksFunction } from "@remix-run/node";
@@ -29,6 +30,7 @@ export const loader = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
 
   return (
     <html lang="en">
@@ -86,7 +88,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          className={navigation.state === "loading" ? "loading" : ""}
+          id="detail"
+        >
           <Outlet />
         </div>
 
